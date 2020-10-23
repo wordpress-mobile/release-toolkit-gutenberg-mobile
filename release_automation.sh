7a97d3d0e8a275dfdef077cf45fdc66f620d4b75
Ceyhun Ozugur <ceyhunozugur@gmail.com>
2020-10-23 20:00:59 +0200

    Add option to cherry-pick a commit from gutenberg before bundling

--- release_automation.sh
+++ release_automation.sh
@@ -146,6 +146,15 @@ else
 fi
 cd ..
 
+# Ask if a cherry-pick is needed before bundling (for example if this is a hotfix release)
+cd gutenberg
+read -p "Do you want to cherry-pick a commit from gutenberg? (y/n) " -n 1
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    read -p "Enter the commit hash to cherry-pick: " GUTENBERG_COMMIT_HASH_TO_CHERRY_PICK
+    execute "git" "cherry-pick" "$GUTENBERG_COMMIT_HASH_TO_CHERRY_PICK"
+fi
+cd ..
 
 # Update the bundles
 ohai "Update the bundles"
