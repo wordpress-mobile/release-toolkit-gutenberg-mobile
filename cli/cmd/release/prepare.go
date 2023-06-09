c0a2250cfda7b3ac90d72a8170a38a27735bb4a2
jhnstn <jhnstn@pm.me>
2023-06-09 18:41:26 -0400

    Update prepare command
    adds options to include gbm and the integration stesps

--- cli/cmd/release/prepare.go
+++ cli/cmd/release/prepare.go
@@ -27,9 +27,25 @@ var PrepareCmd = &cobra.Command{
 
 		var err error
 
+		runIntegration := Apps || Android || Ios
+
+		if Gbm || runIntegration {
+			utils.LogInfo("📦 Running full release pipeline. Let's go! 🚀")
+		}
+
 		gbpr, _ := release.CreateGbPR(version, TempDir, true)
 
 		utils.LogInfo("🏁 Gutenberg release ready to go, check it out: %s", gbpr.Url)
+
+		if Gbm {
+			gbmpr, _ := release.CreateGbmPr(version, TempDir, true)
+
+			utils.LogInfo("🏁 Gutenberg Mobile release ready to go, check it out: %s", gbmpr.Url)
+		}
+
+		if runIntegration {
+			IntegrateCmd.Run(cmd, []string{version})
+		}
 
 		utils.LogDebug("✔️ Done with %s", TempDir)
 
@@ -41,5 +57,9 @@ var PrepareCmd = &cobra.Command{
 }
 
 func init() {
+	PrepareCmd.Flags().BoolVarP(&Gbm, "gbm", "", false, "prepare gutenberg mobile pr")
+	PrepareCmd.Flags().BoolVarP(&Apps, "integrate", "", false, "prepare ios and android prs")
+	PrepareCmd.Flags().BoolVarP(&Android, "android", "", false, "prepare android pr")
+	PrepareCmd.Flags().BoolVarP(&Ios, "ios", "", false, "prepare ios pr")
 	IntegrateCmd.Flags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
 }
