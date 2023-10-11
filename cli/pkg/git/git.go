f3bbb6f75150a5b26f856e9e1f87e1d8e09b0907
Derek Blank <derekpblank@gmail.com>
2023-10-11 16:17:44 +1000

    Add git submodule utility functions

--- cli/pkg/git/git.go
+++ cli/pkg/git/git.go
@@ -55,3 +55,58 @@ func CommitAll(dir, format string, args ...interface{}) error {
 func Push(dir, branch string) error {
 	return exec.ExecGit(dir, true)("push", "origin", branch)
 }
+
+func GetSubmodule(r gh.Repo, path string) (*g.Submodule, error) {
+	w, err := r.Worktree()
+	if err != nil {
+		return nil, err
+	}
+
+	return w.Submodule(path)
+}
+
+func CommitSubmodule(dir, message, submodule string, verbose bool) error {
+	git := exec.ExecGit(dir, verbose)
+
+	if err := git("add", submodule); err != nil {
+		return fmt.Errorf("unable to add submodule %s in %s :%s", submodule, dir, err)
+	}
+
+	if err := git("commit", "-m", message); err != nil {
+		return fmt.Errorf("unable to commit submodule update %s : %s", submodule, err)
+	}
+	return nil
+}
+
+func IsSubmoduleCurrent(s gh.Submodule, expectedHash string) (bool, error) {
+	// Check if the submodule is porcelain
+	sr, err := s.Repository()
+	if clean, err := IsPorcelain(sr); err != nil {
+		return false, err
+	} else if !clean {
+		return false, &NotPorcelainError{fmt.Errorf("submodule %s is not clean", s.Config().Name)}
+	}
+
+	if err != nil {
+		return false, err
+	}
+	stat, err := s.Status()
+	if err != nil {
+		return false, err
+	}
+	eh := plumbing.NewHash(expectedHash)
+
+	return stat.Current == eh, nil
+}
+
+func IsPorcelain(r gh.Repo) (bool, error) {
+	w, err := r.Worktree()
+	if err != nil {
+		return false, err
+	}
+	status, err := w.Status()
+	if err != nil {
+		return false, err
+	}
+	return status.IsClean(), nil
+}
