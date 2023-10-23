4f3bfffda8f0ba43b45c7273d55ae80d49d8d544
jhnstn <jhnstn@pm.me>
2023-10-23 15:24:18 -0400

    Move setup steps to a preflight function

--- cli/cmd/release/prepare/root.go
+++ cli/cmd/release/prepare/root.go
@@ -1,14 +1,19 @@
 package prepare
 
 import (
+	"errors"
+
 	"github.com/spf13/cobra"
 	"github.com/wordpress-mobile/gbm-cli/cmd/utils"
 	wp "github.com/wordpress-mobile/gbm-cli/cmd/workspace"
+	"github.com/wordpress-mobile/gbm-cli/pkg/console"
+	"github.com/wordpress-mobile/gbm-cli/pkg/gbm"
 )
 
 var exitIfError func(error, int)
 var keepTempDir bool
 var workspace wp.Workspace
+var tempDir, version string
 
 var PrepareCmd = &cobra.Command{
 	Use:   "prepare",
@@ -24,13 +29,29 @@ func Execute() {
 	defer workspace.Cleanup()
 }
 
+// Set up the temp directory and version
+// Also validate Aztec versions
+func preflight(args []string) {
+	var err error
+	tempDir = workspace.Dir()
+	version, err = utils.GetVersionArg(args)
+	exitIfError(err, 1)
+
+	// Validate Aztec version
+	if valid := gbm.ValidateAztecVersions(); !valid {
+		exitIfError(errors.New("invalid Aztec versions found"), 1)
+	}
+}
+
 func init() {
 	var err error
 	workspace, err = wp.NewWorkspace()
 	utils.ExitIfError(err, 1)
 
 	exitIfError = func(err error, code int) {
+
 		if err != nil {
+			console.Error(err)
 			utils.Exit(code, workspace.Cleanup)
 		}
 	}
