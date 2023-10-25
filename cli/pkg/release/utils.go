8b7e3d028c9d8bdfd3a83bdcf49a89bd505edb6a
jhnstn <jhnstn@pm.me>
2023-10-25 16:10:16 -0400

    Remove old package version updates

--- cli/pkg/release/utils.go
+++ cli/pkg/release/utils.go
@@ -1,32 +1,13 @@
 package release
 
 import (
-	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
 
-	"github.com/wordpress-mobile/gbm-cli/pkg/gh"
 	"github.com/wordpress-mobile/gbm-cli/pkg/console"
-	"github.com/wordpress-mobile/gbm-cli/pkg/shell"
-	"github.com/wordpress-mobile/gbm-cli/pkg/utils"
+	"github.com/wordpress-mobile/gbm-cli/pkg/gh"
 )
-
-func updatePackageJson(dir, version string, pkgs ...string) error {
-	sp := shell.CmdProps{Dir: dir, Verbose: true}
-	git := shell.NewGitCmd(sp)
-
-	for _, pkg := range pkgs {
-		if err := utils.UpdatePackageVersion(version, filepath.Join(dir, pkg)); err != nil {
-			return err
-		}
-	}
-	if err := git.CommitAll("Release script: Update package.json versions to %s", version); err != nil {
-		return err
-	}
-
-	return nil
-}
 
 type ReleaseChanges struct {
 	Title  string
