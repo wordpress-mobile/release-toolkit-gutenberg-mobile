d2460377d7e24dffa33589d54c37499717ac6818
jhnstn <jhnstn@pm.me>
2023-10-26 16:06:19 -0400

    Make build search more general
    Not it accepts any GBM pr

--- cli/pkg/gbm/builds.go
+++ cli/pkg/gbm/builds.go
@@ -1,18 +1,12 @@
 package gbm
 
 import (
-	"github.com/wordpress-mobile/gbm-cli/pkg/console"
 	"github.com/wordpress-mobile/gbm-cli/pkg/gh"
 )
 
-func AndroidGbmBuildPublished(version string) (bool, error) {
+func AndroidGbmBuildPublished(pr gh.PullRequest) (bool, error) {
 	// Forcing the search to look in the wordpress-mobile org
 	// Since forks will not have the status checks
-	pr, err := FindGbmReleasePr(version)
-	console.Info("Checking status for %s %s", pr.Title, pr.Url)
-	if err != nil {
-		return false, err
-	}
 	sha := pr.Head.Sha
 	status, err := gh.GetStatusCheck("gutenberg-mobile", sha, "build-android-rn-bridge-and-publish-to-s3")
 	if err != nil {
@@ -21,14 +15,9 @@ func AndroidGbmBuildPublished(version string) (bool, error) {
 	return status.State == "success", nil
 }
 
-func IosGbmBuildPublished(version string) (bool, error) {
+func IosGbmBuildPublished(pr gh.PullRequest) (bool, error) {
 	// Forcing the search to look in the wordpress-mobile org
 	// Since forks will not have the status checks
-	pr, err := FindGbmReleasePr(version)
-	console.Info("Checking status for %s %s", pr.Title, pr.Url)
-	if err != nil {
-		return false, err
-	}
 	sha := pr.Head.Sha
 	status, err := gh.GetStatusCheck("gutenberg-mobile", sha, "build-ios-rn-xcframework-and-publish-to-s3")
 	if err != nil {
