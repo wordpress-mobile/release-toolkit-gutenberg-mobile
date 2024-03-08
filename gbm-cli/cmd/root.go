3cfc253de4e31c83bd30c0ea3182d31e5fb69bba
jhnstn <jhnstn@pm.me>
2024-03-08 16:05:08 -0500

    Add prompt to auto update the binary

--- gbm-cli/cmd/root.go
+++ gbm-cli/cmd/root.go
@@ -4,8 +4,8 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/wordpress-mobile/release-toolkit-gutenberg-mobile/gbm-cli/cmd/release"
 	"github.com/wordpress-mobile/release-toolkit-gutenberg-mobile/gbm-cli/cmd/render"
+	"github.com/wordpress-mobile/release-toolkit-gutenberg-mobile/gbm-cli/cmd/utils"
 	"github.com/wordpress-mobile/release-toolkit-gutenberg-mobile/gbm-cli/pkg/console"
-	"github.com/wordpress-mobile/release-toolkit-gutenberg-mobile/gbm-cli/pkg/gh"
 )
 
 const Version = "v1.5.0"
@@ -20,20 +20,11 @@ var rootCmd = &cobra.Command{
 func Execute() {
 	err := rootCmd.Execute()
 	console.ExitIfError(err)
-
 }
 
 func init() {
 	// Add the render command
 	rootCmd.AddCommand(render.RenderCmd)
 	rootCmd.AddCommand(release.ReleaseCmd)
-
-	// Check to see if the user is running the latest version
-	// of the CLI. If not, let them know.
-	latestRelease, err := gh.GetLatestRelease("release-toolkit-gutenberg-mobile")
-	console.ExitIfError(err)
-
-	if latestRelease.TagName != Version {
-		console.Warn("You are running an older version of the CLI. Please update to %s", latestRelease.TagName)
-	}
+	utils.CheckExeVersion(Version)
 }
